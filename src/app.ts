a596f8d2db24a4dc7ef33c3c0591f38e7bdb23cd
falamarcao <falamarcao@gmail.com>
2024-10-21 22:14:09 -0300

    Rendering Project Lists

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,39 @@ function AutoBind(
   return newDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  appElement: HTMLDivElement;
+  element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+      this.templateElement = document.getElementById(
+        'project-list'
+      )! as HTMLTemplateElement;
+      this.appElement = document.getElementById('app')! as HTMLDivElement;
+
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toLocaleUpperCase() + ' PROJECTS';
+    }
+    
+    private attach() {
+        this.appElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -143,3 +176,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
